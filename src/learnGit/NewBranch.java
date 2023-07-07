9deaec40f0f38d7856fd3b878d458b27dd99cb0d
Vikas Viabhav <vikaskricot@gmail.com>
2023-07-07 13:50:19 +0530

    This commit is for learning how to to push in new branch

---
+++ src/learnGit/NewBranch.java
@@ -0,0 +1,11 @@
+package learnGit;
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This class is for learnig of new branch");
+
+	}
+
+}
